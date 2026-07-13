c620ac9d8b5fb048488221927444ec381a5b8310
irpan06 <irvandim8@gmail.com>
2026-07-14 01:28:41 +0700

    dataset preparation

---
+++ scripts/de_engineering/setup_ddl/00_dataset.py
@@ -0,0 +1,25 @@
+import os
+import pandas as pd
+
+# output dir
+output = '../../../dataset'
+
+# dataset url
+url  = 'https://storage.googleapis.com/sql_de/job_postings_flat.csv'
+
+print('read csv file ...')
+df = pd.read_csv(url)
+
+date_column = "job_posted_date"
+
+print('makesure date column correct')
+df[date_column] = pd.to_datetime(df[date_column])
+
+print('start spliting data')
+
+for month, group in df.groupby(df[date_column].dt.month):
+    name_file = f'jobs_month_{month}.csv'
+    path = os.path.join(output, name_file)
+    group = group.sort_values(by=date_column)
+    group.to_csv(path, index=False)
+    print(f'success: {name_file}')
