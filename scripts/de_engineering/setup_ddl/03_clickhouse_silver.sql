e075d7ec58f053132a0d1b5edd1b984508f38801
irpan06 <irvandim8@gmail.com>
2026-07-14 03:35:56 +0700

    create job postings fact table for silver layer

--- scripts/de_engineering/setup_ddl/03_clickhouse_silver.sql
+++ scripts/de_engineering/setup_ddl/03_clickhouse_silver.sql
@@ -32,3 +32,40 @@ CREATE TABLE IF NOT EXISTS wh_silver.dim_date(
 )
 ENGINE = MergeTree()
 ORDER BY date_id;
+CREATE TABLE IF NOT EXISTS wh_silver.dim_date(
+    date_id UInt32,
+    full_date Date,
+    day UInt8,
+    month UInt8,
+    quarter UInt8,
+    year UInt16,
+    week_of_year UInt8,
+    day_of_week UInt8,
+    is_weekend UInt8
+)
+ENGINE = MergeTree()
+ORDER BY date_id;
+
+-- create job posting fact table
+CREATE TABLE IF NOT EXISTS wh_silver.fact_job_postings(
+    job_id Int64,
+    job_hash UInt64,
+    company_id Int64,
+    date_id UInt32,
+    job_title_short String,
+    job_title String,
+    job_location String,
+    job_via String,
+    job_schedule_type String,
+    job_work_from_home UInt8,
+    search_location String,
+    job_no_degree_mention UInt8,
+    job_health_insurance UInt8,
+    job_country String,
+    salary_rate String,
+    salary_year_avg Nullable(Float64),
+    salary_hour_avg Nullable(Float64),
+    refined_at DateTime DEFAULT now()
+)
+ENGINE = MergeTree()
+ORDER BY (date_id, job_title_short);
