885f02ac2fc2c2fd42a810dc714077a3313155db
irpan06 <irvandim8@gmail.com>
2026-07-14 02:53:26 +0700

    create ddl bronze layer

---
+++ scripts/de_engineering/setup_ddl/02_clickhouse_bronze.sql
@@ -0,0 +1,26 @@
+-- create database for bronze layer 
+CREATE DATABASE IF NOT EXISTS src_bronze;
+
+-- create table raw
+CREATE TABLE IF NOT EXISTS src_bronze.raw_data(
+    job_title_short String,
+    job_title String,
+    job_location String,
+    job_via String,
+    job_schedule_type String,
+    job_work_from_home UInt8,
+    search_location String,
+    job_posted_date DateTime,
+    job_no_degree_mention UInt8,
+    job_health_insurance UInt8,
+    job_country String,
+    salary_rate String,
+    salary_year_avg Float64,
+    salary_hour_avg Float64,
+    company_name String,
+    job_skills String,
+    job_type_skills String,
+    inserted_at DateTime DEFAULT now()
+)
+ENGINE = MergeTree()
+ORDER BY (toYYYYMM(job_posted_date), job_title_short);
