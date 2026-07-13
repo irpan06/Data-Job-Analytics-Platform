c38a9af29db962cf017c6d0797c4bf16cc3efeb5
irpan06 <irvandim8@gmail.com>
2026-07-14 03:03:23 +0700

    create skill dim table for silver layer

--- scripts/de_engineering/setup_ddl/03_clickhouse_silver.sql
+++ scripts/de_engineering/setup_ddl/03_clickhouse_silver.sql
@@ -4,9 +4,16 @@ CREATE DATABASE IF NOT EXISTS wh_silver;
 -- create fact and dim table (star schema)
 -- create a company dim table
 CREATE TABLE IF NOT EXISTS wh_silver.dim_company(
-    company_id UInt64,
+    company_id Int64,
     company_name String
 )
 ENGINE = MergeTree()
 ORDER BY company_id;
 
+-- create skill dim table
+CREATE TABLE IF NOT EXISTS wh_silver.dim_skill(
+    skill_id Int64,
+    skill String
+)
+ENGINE = MergeTree()
+ORDER BY skill_id;
