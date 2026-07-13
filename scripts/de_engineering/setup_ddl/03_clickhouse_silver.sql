5e8f49f4f18f52800fe50827471176d3a0ae82d0
irpan06 <irvandim8@gmail.com>
2026-07-14 03:09:16 +0700

    create date dim table for silver layer

--- scripts/de_engineering/setup_ddl/03_clickhouse_silver.sql
+++ scripts/de_engineering/setup_ddl/03_clickhouse_silver.sql
@@ -17,3 +17,18 @@ CREATE TABLE IF NOT EXISTS wh_silver.dim_skill(
 )
 ENGINE = MergeTree()
 ORDER BY skill_id;
+
+-- create date dim table
+CREATE TABLE IF NOT EXISTS wh_silver.dim_date(
+    date_id UInt32,
+    full_date Date,
+    day UInt8,
+    month UInt8,
+    quarter UInt8,
+    year UInt16,
+    week_of_year UInt8,
+    day_of_week UInt8,
+    is_weekend UInt8
+)
+ENGINE = MergeTree()
+ORDER BY date_id;
