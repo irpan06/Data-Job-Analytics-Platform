c6435453bc5c5850d429d969a3561b69c10dafc5
irpan06 <irvandim8@gmail.com>
2026-07-14 05:06:05 +0700

    create database reset script

---
+++ de_engineering/setup_ddl/clickhouse_reset.sql
@@ -0,0 +1,8 @@
+-- drop bronze layer database
+DROP DATABASE IF EXISTS src_bronze;
+
+-- drop silver layer database
+DROP DATABASE IF EXISTS wh_silver;
+
+-- drop gold layer database
+DROP DATABASE IF EXISTS mart_gold;
---
+++ de_engineering/setup_ddl/postgres_reset.sql
@@ -0,0 +1,2 @@
+-- drop table staging (job_postings) in postgres
+DROP TABLE IF EXISTS job_postings;
