474b264eb0edd12c9d33322ca43b03ebe894d76a
irpan06 <irvandim8@gmail.com>
2026-07-14 03:48:10 +0700

    create aggregation salary by role table for gold layer

--- scripts/de_engineering/setup_ddl/04_clickhouse_gold.sql
+++ scripts/de_engineering/setup_ddl/04_clickhouse_gold.sql
@@ -1,2 +1,13 @@
 -- create database for gold layer
 CREATE DATABASE IF NOT EXISTS mart_gold;
+
+-- create aggregation salary by role table
+CREATE TABLE IF NOT EXISTS mart_gold.agg_salary_by_role(
+    job_title_short String,
+    total_postings UInt64,
+    avg_salary_year Float64,
+    avg_salary_hour Float64,
+    updated_at DateTIme DEFAULT now()
+)
+ENGINE = MergeTree()
+ORDER BY job_title_short;
