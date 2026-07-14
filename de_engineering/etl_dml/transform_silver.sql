f99b1a4066cad873d7ac48412bb016c0bd41a7c5
irpan06 <irvandim8@gmail.com>
2026-07-14 10:29:56 +0700

    create etl queries for skill dim table in silver layer

--- de_engineering/etl_dml/transform_silver.sql
+++ de_engineering/etl_dml/transform_silver.sql
@@ -14,6 +14,38 @@ SELECT
 FROM(
     SELECT DISTINCT company_name 
     FROM src_bronze.raw_data
-    WHERE company_name IS NOT NULL AND company_name != ''
+    WHERE company_name IS NOT NULL
+      AND company_name != ''
 )
 WHERE company_name NOT IN (SELECT company_name FROM wh_silver.dim_company);
+
+-- skill dim table
+WITH 
+max_skill_id AS (
+    SELECT ifNull(MAX(skill_id),0) AS max_id
+    FROM wh_silver.dim_skill
+),
+skills AS(
+    SELECT DISTINCT
+        trim(
+            arrayJoin(
+                splitByChar(
+                    ',',
+                    replaceRegexpAll(job_skills, '[\\[\\]\']', '')
+                )
+            )
+        ) AS skill
+    FROM src_bronze.raw_data
+    WHERE job_skills IS NOT NULL
+      AND job_skills != ''
+      AND job_skills != '[]'
+)
+
+INSERT INTO wh_silver.dim_skill(
+    skill_id, skill
+)
+SELECT
+    max_id + row_number() OVER (ORDER BY skill),
+    skill
+FROM max_skill_id, skills
+WHERE skill NOT IN (SELECT skill FROM wh_silver.dim_skill);
