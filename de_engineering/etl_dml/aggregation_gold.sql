05ab1440e8b7a6df452230855bd016cb3efaf68f
irpan06 <irvandim8@gmail.com>
2026-07-15 08:43:43 +0700

    create data mart aggregation skill salary table in gold layer

--- de_engineering/etl_dml/aggregation_gold.sql
+++ de_engineering/etl_dml/aggregation_gold.sql
@@ -34,8 +34,36 @@ ON f.job_id = b.job_id
 INNER JOIN wh_silver.dim_skill s
 ON b.skill_id = s.skill_id
 
+WHERE job_title_short IS NOT NULL 
+ AND job_title_short != ''
+
 GROUP BY
     d.year,
     d.month,
     f.job_title_short,
     skill;
+
+-- aggregation top paying skill
+TRUNCATE TABLE mart_gold.agg_top_paying_skills;
+
+INSERT INTO mart_gold.agg_top_paying_skills(
+    job_title_short, skill, avg_salary_year, total_indexed_jobs
+)
+SELECT
+    f.job_title_short,
+    s.skill,
+    AVG(salary_year_avg) AS avg_salary_year,
+    COUNT(DISTINCT f.job_id) AS total_indexed_jobs
+FROM wh_silver.fact_job_postings f
+
+INNER JOIN wh_silver.bridge_skill_job b
+ON f.job_id = b.job_id
+
+INNER JOIN wh_silver.dim_skill s
+ON b.skill_id = s.skill_id
+
+WHERE f.salary_year_avg IS NOT NULL 
+
+GROUP BY
+    f.job_title_short,
+    s.skill;
